3d96abaeecc99b1d2d5c93158c4046eab5cf5f8b
arielrazumardi <arielrazumardi@gmail.com>
2021-08-15 23:48:47 +0700

    fix minor bug

--- examples/mnist-df.py
+++ examples/mnist-df.py
@@ -155,7 +155,7 @@ def main(_):
               ascent_loss += mon_sess.run(cross_entropy_ascent, feed_dict={x_ascent: [batch_xs[i]], y__ascent: [batch_ys[i]]}) 
           stochastic_sharpness = float(ascent_loss - descent_loss) / batch_size
           if not mon_sess.should_stop():
-            mon_sess.run(concat_to_stochastic_sharpness_list, feed_dict={new_stochastic_sharpness: stochastic_sharpness)
+            mon_sess.run(concat_to_stochastic_sharpness_list, feed_dict={new_stochastic_sharpness: stochastic_sharpness})
           median_sharpness = 0
           if not mon_sess.should_stop():
             median_sharpness = get_median(stochastic_sharpness_list)
