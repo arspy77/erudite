8f6c1abe8365339c090d278a7ada0eaf33406a44
arielrazumardi <arielrazumardi@gmail.com>
2021-08-16 21:37:07 +0700

    edit demo script

--- demo-reset.sh
+++ demo-reset.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-kubectl delete tfjob job1
-kubectl delete tfjob job2
 kubectl delete tfjob job3
 kubectl delete tfjob job4
--- demo.sh
+++ demo.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-kubectl create -f examples/job1.yaml
-kubectl create -f examples/job2.yaml
 kubectl create -f examples/job3.yaml
 kubectl create -f examples/job4.yaml
 kubectl get pods -w
