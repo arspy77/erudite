bc834eff424808acbc6846470965f8e9766949d3
arielrazumardi <arielrazumardi@gmail.com>
2021-08-17 16:08:43 +0700

    change sharpness formula

--- examples/mnist-df-2.py
+++ examples/mnist-df-2.py
@@ -268,15 +268,19 @@ elif FLAGS.job_name == "worker":
                     for i in range(n_descent):
                         sess.run(train_op_descent, feed_dict={x_descent: batch_x, y__descent: batch_y})
                     
-                    descent_loss = 0
-                    ascent_loss = 0
-                    for i in range(batch_size):
-                        descent_loss += sess.run(cross_entropy_descent, feed_dict={x_descent: [batch_x[i]], y__descent: [batch_y[i]]})
-                        ascent_loss += sess.run(cross_entropy_ascent, feed_dict={x_ascent: [batch_x[i]], y__ascent: [batch_y[i]]}) 
+                    # descent_loss = 0
+                    # ascent_loss = 0
+                    # for i in range(batch_size):
+                    #     descent_loss += sess.run(cross_entropy_descent, feed_dict={x_descent: [batch_x[i]], y__descent: [batch_y[i]]})
+                    #     ascent_loss += sess.run(cross_entropy_ascent, feed_dict={x_ascent: [batch_x[i]], y__ascent: [batch_y[i]]}) 
+                    
                     # descent_loss = sess.run(descent_loss_op, feed_dict={x_descent: batch_x, y__descent: batch_y})
                     # ascent_loss = sess.run(ascent_loss_op, feed_dict={x_ascent: batch_x, y__ascent: batch_y})
-                        
-                    stochastic_sharpness = float(ascent_loss - descent_loss) / batch_size
+
+                    descent_loss = sess.run(cross_entropy_descent, feed_dict={x_descent: batch_x, y__descent: batch_y})
+                    ascent_loss = sess.run(cross_entropy_ascent, feed_dict={x_ascent: batch_x, y__ascent: batch_y})
+
+                    stochastic_sharpness = float(ascent_loss - descent_loss) # / batch_size
                     print("asc loss : %3.10f" % ascent_loss)
                     print("desc loss : %3.10f" % descent_loss)
                     print(stochastic_sharpness)
